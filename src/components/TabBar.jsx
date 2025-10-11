f3b2fea7c0d032ed9789748e41490988eab3817e
Andrew Jang <dhj9817@gmail.com>
2025-10-12 06:38:17 +0900

    .

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -25,19 +25,21 @@ const TabBar = ({ user, setUser }) => {
     };
 
     return (
-        <nav className="w-full flex-shrink-0 md:flex justify-center z-30 h-14 items-center md:px-4 bg-white fixed bottom-0 shadow-xl border-t">
-            <div className="flex w-full h-14 justify-between space-x-2">
-                {user && (
-                    <div className="flex flex-shrink-0 space-x-6 items-center justify-evenly md:justify-start w-full h-14">
-                        <MenuButton id="uploads" icon={<IoIosImage className="w-6 h-6" />} />
-                        <MenuButton id="analytics" icon={<SiGoogleanalytics className="w-5 h-5" />} />
-                        <MenuButton id="user" icon={<FaUserLarge className="w-5 h-5" />} />
-                        <MenuButton id="chat" icon={<IoChatbubbleSharp className="w-5 h-5" />} />
-                        <MenuButton id="dev" icon={<FaDev className="w-5 h-5" />} />
-                    </div>
-                )}
-            </div>
+        <nav className="flex-shrink-0 md:flex justify-center z-30 w-12 items-center md:px-4 
+    fixed bottom-2 right-2 shadow-xl border-t rounded-full
+    bg-white/60 backdrop-blur-xl">
+            {user && (
+                <div className="flex flex-col flex-shrink-0 items-center justify-evenly md:justify-start">
+                    <MenuButton id="dev" icon={<FaDev className="w-5 h-5" />} />
+                    <MenuButton id="chat" icon={<IoChatbubbleSharp className="w-5 h-5" />} />
+                    <MenuButton id="user" icon={<FaUserLarge className="w-5 h-5" />} />
+                    <MenuButton id="uploads" icon={<IoIosImage className="w-6 h-6" />} />
+                    <MenuButton id="analytics" icon={<SiGoogleanalytics className="w-5 h-5" />} />
+
+                </div>
+            )}
         </nav>
+
     );
 };
 
